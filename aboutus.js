e543c000ca26ec1d8f939435c1435a7200e0b1a5
Sam Davey Palisoc <palisocsamdavey@gmail.com>
2026-05-10 12:08:28 +0800

    Add files via upload

---
+++ aboutus.js
@@ -0,0 +1,5 @@
+console.log("About Page Loaded Successfully");
+
+document.getElementById("slides").addEventListener("click", function () {
+    document.getElementById("mobile-menu").classList.toggle("open");
+});
